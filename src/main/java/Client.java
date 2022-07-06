51f3b59c097b319fae6529268f49ea777a94d75e
alex-drum <alex_drum@mail.ru>
2022-07-06 18:22:42 +0300

    Проверка на предмет нового пользователя (2)

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,14 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-//    public static void setAuthorized(boolean authorized) {
-//        Client.authorized = authorized;
-//    }
-
-//    static boolean authorized;
+    private static final int PORT = 8189;
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 8189);
+            Socket socket = new Socket("localhost", PORT);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
@@ -19,55 +16,64 @@ public class Client {
             if (isNewUser) {
                 boolean isNameVacant = isNameVacant();
             }
-            out.writeUTF("/auth login1 pass1");
-//            setAuthorized(false);
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (true) {
-                            if (in.available() > 0) {
-                                String strFromServer = in.readUTF();
-                                if (strFromServer.startsWith("/authOk")) {
-//                                    setAuthorized(true);
-                                    System.out.println("Authorized on server");
-                                    Client.runOutputThread(out);
-                                    break;
-                                }
-                                System.out.println(strFromServer + "\n");
-                            }
-                        }
-                        while (true) {
-                            if (in.available() > 0) {
-                                String strFromServer = in.readUTF();
-                                if (strFromServer.equalsIgnoreCase("/end")) {
-                                    break;
-                                }
-                                System.out.println(strFromServer);
-                                System.out.println("\n");
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            t.start();
-            t.join();
-        } catch (IOException | InterruptedException e) {
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+//            out.writeUTF("/auth login1 pass1");
+//            setAuthorized(false);
+//            Thread t = new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    try {
+//                        while (true) {
+//                            if (in.available() > 0) {
+//                                String strFromServer = in.readUTF();
+//                                if (strFromServer.startsWith("/authOk")) {
+////                                    setAuthorized(true);
+//                                    System.out.println("Authorized on server");
+//                                    Client.runOutputThread(out);
+//                                    break;
+//                                }
+//                                System.out.println(strFromServer + "\n");
+//                            }
+//                        }
+//                        while (true) {
+//                            if (in.available() > 0) {
+//                                String strFromServer = in.readUTF();
+//                                if (strFromServer.equalsIgnoreCase("/end")) {
+//                                    break;
+//                                }
+//                                System.out.println(strFromServer);
+//                                System.out.println("\n");
+//                            }
+//                        }
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//            t.start();
+//            t.join();
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+        }
 
-    private static boolean isNameVacant() {
+
+        private static boolean isNameVacant() {
         System.out.println("Please enter your nickname: ");
         String userName = null;
         try {
             userName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            Socket socket = new Socket("localhost", PORT);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(userName);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(userName);
+
         return false;
     }
 
--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,3 +1,6 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -5,47 +8,60 @@ import java.net.Socket;
 
 public class ClientHandler {
 
-        private Server server;
-        private Socket socket;
-        private DataInputStream in;
-        private DataOutputStream out;
-//        private String name;
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
 
-//        public String getName() {
-//            return name;
-//        }
-
-        public ClientHandler(Server server, Socket socket) {
-            this.server = server;
-            this.socket = socket;
-//            this.name = "";
+    public ClientHandler(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+        new Thread(() -> {
             try {
                 this.in = new DataInputStream(socket.getInputStream());
                 this.out = new DataOutputStream(socket.getOutputStream());
-                new Thread(()-> {
-                    try {
+               while (true) {
+                   if (in.available() > 0) {
+                       String nameToCheck = in.readUTF();
+                       boolean isNameVacant = isNameVacant(nameToCheck);
+                       System.out.println(nameToCheck + "is vacant? - " + isNameVacant);
+                   }
+               }
 //                        authenticate();
 //                        readMessages();
-                    }
-                    finally {
-                        closeConnection();
-                    }
-                }).start();
-            } catch (IOException ex) {
-                throw new RuntimeException("Client creation error");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//                    finally {             // TODO потом надо понять, в какой момент раскомментить
+//                        closeConnection();
+//                    }
+        }).start();
+    }
+
+
+    private boolean isNameVacant(String userName) {
+        JSONArray jsonArray = server.getResultsJSONArray(server.fetchQuery);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if (userName.equals(jsonObject.getString("name"))) {
+                System.out.println("This name is occupied");
+                return false;
             }
         }
+        System.out.println("This name is vacant");
+        return true;
+    }
 
-        private void closeConnection() {
-            try {
-                in.close();
-                out.close();
-                socket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+    private void closeConnection() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 //            myServer.unsubscribe(this);
 //            myServer.broadcast("User " + name + " left");
-        }
+    }
 
 }
