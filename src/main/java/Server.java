2016f1ca12e1acdb1c0a56fe40de113eecbf3009
alex-drum <alex_drum@mail.ru>
2022-07-06 14:32:20 +0300

    добавил подключение к БД

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,11 +1,23 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
     private final int PORT = 8189;
+
+    private static final String url = "jdbc:mysql://localhost/testDB";
+    private static final String dbUser = "root";
+    private static final String dbPassword = "haizi2011";
+
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
 //    private List<ClientHandler> clients;
 //    private AuthService authService;
 //
@@ -24,6 +36,32 @@ public class Server {
         } catch (IOException ex) {
             System.out.println("Server error");
         }
+    }
+
+    public JSONArray getResultsJSONArray(String sql){
+        java.sql.Statement statement;
+        JSONArray jsonArray = new JSONArray();
+
+        try {
+            con = DriverManager.getConnection(url, dbUser, dbPassword);
+            statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            Integer i = 0;
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            while (rs.next()){
+                JSONObject jsonObject = new JSONObject();
+                for (int j = 1; j < columnCount + 1; j++ ) {
+                    String key = rsmd.getColumnLabel(j);
+                    jsonObject.put(key, rs.getObject(key));
+                }
+                jsonArray.put(i, jsonObject);
+                i++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
     }
 
 
